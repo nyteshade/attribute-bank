6f4ebecaafda254fc7c9df59d4a03e026c6e3876
Brielle Harrison <nyteshade@gmail.com>
2024-06-18 15:55:58 -0600

    Initial Commit

---
+++ AttributeBank/AttributeBankApp.swift
@@ -0,0 +1,17 @@
+//
+//  AttributeBankApp.swift
+//  AttributeBank
+//
+//  Created by Brielle Harrison on 6/18/24.
+//
+
+import SwiftUI
+
+@main
+struct AttributeBankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
